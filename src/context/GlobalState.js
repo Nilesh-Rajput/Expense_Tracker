aa3f805f976d1490e49f26eb84274702aac2ed05
Nilesh Rajput <nileshrajput3389@gmail.com>
2024-06-05 15:11:23 +0530

    adding to github

---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, {createContext, useReducer} from "react";
+import AppReducer from "./AppReducer";
+
+//Initial State
+const initialState = {
+    transcations: [
+       
+    ]
+}
+
+//create Context
+export const GlobalContext = createContext(initialState);
+
+//Provider Component 
+export const GlobalProvider = ({children}) => {
+    const [state, dispatch] = useReducer(AppReducer,initialState);
+
+    //Actions
+    function deleteTranscation(id){
+        dispatch({
+            type: 'DELETE_TRANSCATION',
+            payload: id
+        });
+    }
+
+    function addTranscation(transcation){
+        dispatch({
+            type: 'ADD_TRANSCATION',
+            payload: transcation
+        });
+    }
+
+    return (
+        <GlobalContext.Provider value={{
+            transcations: state.transcations,
+            deleteTranscation,
+            addTranscation
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
